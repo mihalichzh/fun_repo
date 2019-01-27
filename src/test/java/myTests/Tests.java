eea0df32c8fb09cc4c9252b5d51867aa1768dd2e
Mikhail Zheltyshev <test@test.com>
2019-01-27 11:03:22 +0300

    Prepared for test

--- src/test/java/myTests/Tests.java
+++ src/test/java/myTests/Tests.java
@@ -1,10 +1,12 @@
 package myTests;
 
 import helpers.FileHelper;
+import org.apache.commons.io.FileUtils;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.io.File;
+import java.io.IOException;
 
 public class Tests {
 
@@ -13,5 +15,12 @@ public class Tests {
         File file = new File(FileHelper.getCurrentFolder(),"test.txt");
         System.out.println(FileHelper.getCurrentFolder());
         Assertions.assertTrue(file.exists());
+    }
+
+    @Test
+    void testFileContentIsExpected() throws IOException {
+        File actual = new File(FileHelper.getCurrentFolder(),"test.txt");
+        File expected = new File("src/test/resources","test.txt");
+        Assertions.assertTrue(FileUtils.contentEquals(actual,expected));
     }
 }
